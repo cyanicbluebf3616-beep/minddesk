13921de5504c4857658b86042dcdf9ae803a21a8
cyanicbluebf3616-beep <cyanicblue.bf3616@gmail.com>
2026-04-21 13:19:50 +0900

    Delete script directory

--- script/priority.js
+++
@@ -1,7 +0,0 @@
-function calculateTaskPriority(task) {
-  return {
-    score: getRiskScore(task),
-    label: getRiskScore(task) >= 55 ? "最優先" : getRiskScore(task) >= 25 ? "高" : "通常",
-    reasons: getRiskReasons(task)
-  };
-}
